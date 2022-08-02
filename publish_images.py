a7e361e6f12bc231c3867de293e8f17939c29246
Danil Nikitin <d.nikitin.dev@gmail.com>
2022-08-02 19:28:08 +0300

    Add publish photo to Telegram channel.

--- publish_images.py
+++ publish_images.py
@@ -8,6 +8,10 @@ def main() -> None:
     settings = Settings()
     bot = telegram.Bot(token=settings.TG_BOT_TOKEN)
     bot.send_message(chat_id=settings.TG_CHAT_ID, text="Hello, world!")
+    bot.send_document(
+        chat_id=settings.TG_CHAT_ID,
+        document=open("images/space_x_1.jpg", mode="rb")
+    )
 
 
 if __name__ == "__main__":
