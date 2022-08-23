959271d2a13e5c697c6bedcdab40770d694aab64
Danil Nikitin <d.nikitin.dev@gmail.com>
2022-08-23 11:36:12 +0300

    Refactor code. Simplified the code in the if condition for fetch_nasa_apod_images.py.

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -39,30 +39,31 @@ def fetch_nasa_apod(images_count: int) -> None:
     apod = apod.json()
 
     for media_number, media_link in enumerate(apod, start=1):
-        if media_link["media_type"] == "image":  # type: ignore
-            image_link = media_link["url"]  # type: ignore
-            file_name = "nasa_apod_"
-            file_extension = get_file_extension(url=image_link)
-            apod_nasa_path = sanitize_file_path(
-                file_path=settings.IMG_PATH,
-                file_name=f"{file_name}{media_number}{file_extension}"
-            )
-            try:
-                load_image(url=str(image_link), file_path=apod_nasa_path)
-                message = f"""{media_number})Фото по ссылке: {image_link!r}
-                        было загружено по пути: {apod_nasa_path}
-                """
-                click.echo(message=correct_textwrap_dedent(message))
+        if media_link["media_type"] != "image":  # type: ignore
+            continue
+        image_link = media_link["url"]  # type: ignore
+        file_name = "nasa_apod_"
+        file_extension = get_file_extension(url=image_link)
+        apod_nasa_path = sanitize_file_path(
+            file_path=settings.IMG_PATH,
+            file_name=f"{file_name}{media_number}{file_extension}"
+        )
+        try:
+            load_image(url=str(image_link), file_path=apod_nasa_path)
+            message = f"""{media_number})Фото по ссылке: {image_link!r}
+                    было загружено по пути: {apod_nasa_path}
+            """
+            click.echo(message=correct_textwrap_dedent(message))
 
-            except HTTPError as err:
-                message = f"{media_number})Фото по ссылке: {image_link!r} " \
-                          f"{err}"
-                click.echo(message=message)
+        except HTTPError as err:
+            message = f"{media_number})Фото по ссылке: {image_link!r} " \
+                        f"{err}"
+            click.echo(message=message)
 
-            except ConnectionError as err:
-                message = f"Ошибка подключения :( {err}"
-                click.echo(message=message)
-                time.sleep(settings.TIMEOUT)
+        except ConnectionError as err:
+            message = f"Ошибка подключения :( {err}"
+            click.echo(message=message)
+            time.sleep(settings.TIMEOUT)
 
 
 if __name__ == "__main__":
